ebbbaf3993acbc2389b91241a7ca660544c230bd
Varoon Enjeti <97761722+varoon-enjeti@users.noreply.github.com>
2023-05-28 14:30:41 -0500

    Initial Commit

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Varoon Enjeti on 5/28/23.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
